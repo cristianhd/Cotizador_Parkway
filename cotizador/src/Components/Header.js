f1fd8a077beae4b4651d8780f83fe8f976a4963c
cristianhd <cristian.hd1996@gmail.com>
2022-01-03 18:05:28 -0300

    header img background responsive cover

--- cotizador/src/Components/Header.js
+++ cotizador/src/Components/Header.js
@@ -1,19 +1,16 @@
 import React from "react";
-import "../style/header.css";
 import logo from "../assets/header/Logo.png";
-import background from "../assets/header/Fondo.png";
+
+import "../style/header.css";
 
 export default function Header() {
   return (
-    <div>
-      <header>
-          <img src={background} alt="logo" className="img-back" />
-          <div className="logo">
-
-          <img src={logo} alt="logo" className="img-logo" />
-          </div>
-          
-      </header>
-    </div>
+    <header>
+      <div className="cont-back"></div>
+      <div className="logo">
+        <img src={logo} alt="logo" className="img-logo" />
+        {/* <img src={} alt="down-buttom"/> */}
+      </div>
+    </header>
   );
 }
