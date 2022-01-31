a4246774c1b8fe17a3dbb1710ac81069fca55a57
cristianhd <cristian.hd1996@gmail.com>
2022-01-30 22:42:27 -0300

    fixed entrada-salida

--- cotizador/src/Components/DatePicker.js
+++ cotizador/src/Components/DatePicker.js
@@ -6,14 +6,14 @@ export default function DatePicker() {
   return (
     <div className="date-wrap d-flex flex-row">
       <div className="check-in">
-        <span>Check-out</span>
+        <span>Entrada</span>
         <Form.Control
           className="input-date"
           placeholder="20-01-2022"
         ></Form.Control>
       </div>
       <div className="check-out">
-        <span>Check-in</span>
+        <span>Salida</span>
         <Form.Control
           className="input-date"
           placeholder="20-01-2022"
