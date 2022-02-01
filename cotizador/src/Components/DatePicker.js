4a97c469fc49daab8bdaec216b325fb7ed228306
cristianhd <cristian.hd1996@gmail.com>
2022-02-01 15:58:25 -0300

    finish daterangepicker

--- cotizador/src/Components/DatePicker.js
+++ cotizador/src/Components/DatePicker.js
@@ -1,24 +1,29 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import "../style/datePicker.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-daterangepicker/daterangepicker.css";
+import DateRangePicker from "react-bootstrap-daterangepicker";
 
 export default function DatePicker() {
+  const tiempoTranscurrido = Date.now();
+  const hoy = new Date(tiempoTranscurrido);
+
   return (
-    <div className="date-wrap d-flex flex-row">
-      <div className="check-in">
-        <span>Entrada</span>
-        <Form.Control
-          className="input-date"
-          placeholder="20-01-2022"
-        ></Form.Control>
+    <div className="date-wrap d-flex flex-colum">
+      <div className="dates">
+        <div className="d-flex flex-row justify-content-around">
+          <span>Entrada</span>
+          <span>Salida</span>
+        </div>
+        <div className="picker">
+
+        <DateRangePicker initialSettings={{ startDate: hoy }}>
+          <Form.Control type="text" className="input-c">
+          </Form.Control>
+        </DateRangePicker>
       </div>
-      <div className="check-out">
-        <span>Salida</span>
-        <Form.Control
-          className="input-date"
-          placeholder="20-01-2022"
-        ></Form.Control>
-      </div>
+        </div>
     </div>
   );
 }
