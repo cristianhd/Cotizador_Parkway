db6563d350cad6d1535c561046f1f1f4ef8f4b87
cristianhd <cristian.hd1996@gmail.com>
2022-02-08 20:54:59 -0300

    mongo config

---
+++ cotizador/api/mongo.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose")
+const connectionString = process.env.CONNECTION_STRING;
+console.log(connectionString)
+
+mongoose.connect(connectionString).then(()=>{
+    console.log("Database Connected");
+}).catch(err=>{
+    console.error(err)
+})
