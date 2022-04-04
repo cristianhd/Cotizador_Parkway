a8f583344ab88f7dd14ec02b43135136814094de
cristianhd <cristian.hd1996@gmail.com>
2022-04-04 15:44:53 -0300

    initial configuration store and reducer Redux

---
+++ cotizador/src/Redux/reducer/index.js
@@ -0,0 +1,15 @@
+
+
+const intialState={
+    data: []
+}
+
+export function rootReducer(state= intialState, action){
+    switch (action.type){
+    
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
