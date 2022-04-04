c3337ef678c0c0bf4885d261b027ff7ba19e3ebf
cristianhd <cristian.hd1996@gmail.com>
2022-04-03 21:51:55 -0300

    create controller to find and post products

--- cotizador/api/controllers/products.js
+++ cotizador/api/controllers/products.js
@@ -1,10 +1,11 @@
 const Experiencia = require("../models/Experiencias");
+const Hospedaje = require("../models/Hospedajes");
 
 function findExperiencias(req, res, next) {
-  const typeProduct = req.params.type;
+  const typeProduct = req.params.type.toLowerCase();
 
   if (typeProduct !== "Experiencias") {
-    console.log(req.params.type);
+    next();
   }
 
   try {
@@ -17,21 +18,62 @@ function findExperiencias(req, res, next) {
 }
 
 function findHospedaje(req, res, next) {
-    const typeProduct = req.params.type;
-  
-    if (typeProduct !== "Hospedaje") {
-      console.log(req.params.type);
-    }
-  
-    try {
-      Hospedaje.find().then((Experiencias) => {
-        res.json(Experiencias);
-      });
-    } catch (error) {
-      res.send(error.message);
-    }
+  const typeProduct = req.params.type.toLowerCase();
+
+  if (typeProduct !== "hospedaje") {
+    console.log(req.params.type);
   }
+
+  try {
+    Hospedaje.find().then((Hospedaje) => {
+      res.json(Hospedaje);
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+}
+
+function postExperiencia(req, res, next) {
+  const typeProduct = req.params.type.toLowerCase();
+
+  if (typeProduct !== "experiencias") {
+    next();
+  }
+  console.log(req.body);
+  console.log(typeProduct);
+  const experiencias = req.body;
+
+  try {
+    Experiencia.insertMany(experiencias).then((result) => {
+      res.json(result);
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+}
+
+function postHospedajes(req, res, next) {
+  const typeProduct = req.params.type.toLowerCase();
+
+  if (typeProduct !== "hospedajes") {
+    console.log(req.params.type);
+  }
+  console.log(req.body);
+  console.log(typeProduct);
+  const hospedajes = req.body;
+
+  try {
+    Hospedaje.insertMany(hospedajes).then( async (result) => {
+      await res.json(result);
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+}
 
 module.exports = {
   findExperiencias,
+  findHospedaje,
+  postExperiencia,
+  postHospedajes,
 };
--- cotizador/api/routes/products.js
+++ cotizador/api/routes/products.js
@@ -1,7 +1,14 @@
-const { Router } = require('express');
-const { findExperiencias } = require('../controllers/products.js');
+const { Router } = require("express");
+const Experiencia = require("../models/Experiencias")
+const {
+  findExperiencias,
+  findHospedaje,
+  postExperiencia,
+  postHospedajes,
+} = require("../controllers/products.js");
 const router = Router();
 
-router.get('/:type',findExperiencias);
+router.get("/:type", findExperiencias, findHospedaje);
+router.post("/:type",postExperiencia,postHospedajes);
 
 module.exports = router;
