9979fd921c20f7c031231dcd4108a5140a7443c7
cristianhd <cristian.hd1996@gmail.com>
2022-06-05 17:18:03 -0300

    create habdle save and change

--- cotizador/src/Components/ModalCategoryUser.js
+++ cotizador/src/Components/ModalCategoryUser.js
@@ -3,24 +3,38 @@ import Modal from "react-bootstrap/esm/Modal";
 import Button from "react-bootstrap/esm/Button";
 import Form from "react-bootstrap/esm/Form";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useDispatch } from "react-redux";
+import { createUser } from "../Redux/action";
 
 export default function ModalCategoryUser() {
   const [show, setShow] = useState(true);
-  const [categoryUser,setCategoryUser] = useState(null)
-  const { user } = useAuth0();
+  const [categoryUser, setCategoryUser] = useState(null);
+  const { user, getAccessTokenSilently } = useAuth0();
+  const dispatch = useDispatch();
 
-  const handleClose = () => {
-    
+  const { name, nickname, sub, picture, email } = user;
+  const category = categoryUser;
+  const data = {
+    name,
+    nickname,
+    sub,
+    category,
+    picture,
+    email
+  };
+  const handleSave = async() => {
+    const token = await getAccessTokenSilently();
+    console.log(data)
+    dispatch(createUser(data,token));
     setShow(false);
-  
-  }
+  };
 
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     const category = e.target.value;
-    setCategoryUser(category)
-  }
-  
-  console.log(user,categoryUser)
+    setCategoryUser(category);
+  };
+
+  console.log("data", data);
 
   return (
     <div>
@@ -33,6 +47,7 @@ export default function ModalCategoryUser() {
             <Form.Group>
               <Form.Label>Elije tu categoria</Form.Label>
               <Form.Select onChange={handleChange}>
+              <option>Selecciona</option>
                 <option value="Proveedor">Proveedor</option>
                 <option value="Cotizador">Cotizador</option>
                 <option value="Asesor">Asesor</option>
@@ -41,7 +56,7 @@ export default function ModalCategoryUser() {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleSave}>
             Guardar
           </Button>
         </Modal.Footer>
