2ad0583bbea0b1ff5479d4e86272bb3ee98ea41d
cristianhd <cristian.hd1996@gmail.com>
2022-07-05 01:15:34 -0300

    Room Form restructure

--- cotizador/src/Components/Planes/RoomForm.js
+++ cotizador/src/Components/Planes/RoomForm.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Col, Form } from "react-bootstrap";
+import { Col, Form, InputGroup } from "react-bootstrap";
 
 export default function RoomForm({ handleOnChangeRoom }) {
   const [checkedItem, setCheckedItem] = useState({});
-  const [currentChecked, setCurrentChecked] = useState(["sencilla"]);
+  const [currentChecked, setCurrentChecked] = useState([]);
   const [room, setRoom] = useState({});
-  const [valueRoom, setValueRoom] = useState([]);
+  const Regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/g;
 
   function handleOnChangeCheckbox(e) {
     const name = e.target.name;
@@ -25,7 +25,7 @@ export default function RoomForm({ handleOnChangeRoom }) {
 
   function handleOnChangeControl(e) {
     const name = e.target.name;
-    const price = e.target.value;
+    const price = e.target.value.replace(/[.$]/g, "");
 
     setRoom({
       ...room,
@@ -34,108 +34,91 @@ export default function RoomForm({ handleOnChangeRoom }) {
     handleOnChangeRoom(room);
   }
 
-  // useEffect(() => {
-  //   for (const type in room) {
-  //     setValueRoom(...valueRoom, { type: type, price: room[type] });
-  //   }
-  //   console.log(valueRoom)
-  // }, [valueRoom,room]);
-
-  console.log(room, currentChecked);
   return (
     <>
       <Form.Label>ACOMODACIÓN</Form.Label>
-      <Form.Group as={Col} className="p-3">
+      <InputGroup hasValidation as={Col} className="p-3">
         <Form.Check
           name="sencilla"
           type="checkbox"
           label="Sencilla"
-          checked
           onChange={handleOnChangeCheckbox}
-        />
-        <Form.Control
           required
-          type="text"
-          name="sencilla"
-          placeholder="$"
-          onChange={handleOnChangeControl}
-          disabled={!currentChecked.includes("sencilla")}
         />
-      </Form.Group>
-      <Form.Group as={Col} className="p-3">
+        <InputGroup>
+          <InputGroup.Text>$ COP</InputGroup.Text>
+          <Form.Control
+            name="sencilla"
+            onChange={handleOnChangeControl}
+            disabled={!currentChecked.includes("sencilla")}
+            required
+          />
+        </InputGroup>
+      </InputGroup>
+      <InputGroup hasValidation as={Col} className="p-3">
         <Form.Check
           name="doble"
           type="checkbox"
           label="Doble"
           onChange={handleOnChangeCheckbox}
         />
-        <Form.Control
-          type="text"
-          name="doble"
-          placeholder="$"
-          onChange={handleOnChangeControl}
-          disabled={!currentChecked.includes("doble")}
-        />
-      </Form.Group>
-      <Form.Group as={Col} className="p-3">
+        <InputGroup>
+          <InputGroup.Text>$ COP</InputGroup.Text>
+          <Form.Control
+            name="doble"
+            onChange={handleOnChangeControl}
+            disabled={!currentChecked.includes("doble")}
+          />
+        </InputGroup>
+      </InputGroup>
+      <InputGroup hasValidation as={Col} className="p-3">
         <Form.Check
           name="triple"
           type="checkbox"
           label="Triple"
           onChange={handleOnChangeCheckbox}
         />
-        <Form.Control
-          type="text"
-          name="triple"
-          placeholder="$"
-          onChange={handleOnChangeControl}
-          disabled={!currentChecked.includes("triple")}
-        />
-      </Form.Group>
-      <Form.Group as={Col} className="p-3">
+        <InputGroup>
+          <InputGroup.Text>$ COP</InputGroup.Text>
+          <Form.Control
+            name="triple"
+            onChange={handleOnChangeControl}
+            disabled={!currentChecked.includes("triple")}
+          />
+        </InputGroup>
+      </InputGroup>
+      <InputGroup hasValidation as={Col} className="p-3">
         <Form.Check
           name="cuadruple"
           type="checkbox"
           label="Cuadruple"
           onChange={handleOnChangeCheckbox}
         />
-        <Form.Control
-          type="text"
-          name="cuadruple"
-          placeholder="$"
-          onChange={handleOnChangeControl}
-          disabled={!currentChecked.includes("cuadruple")}
-        />
-      </Form.Group>
-      <Form.Group as={Col} className="p-3">
+        <InputGroup>
+          <InputGroup.Text>$ COP</InputGroup.Text>
+          <Form.Control
+            name="cuadruple"
+            onChange={handleOnChangeControl}
+            disabled={!currentChecked.includes("cuadruple")}
+          />
+        </InputGroup>
+      </InputGroup>
+      <InputGroup hasValidation as={Col} className="p-3">
         <Form.Check
           name="quintuple"
           type="checkbox"
           label="Quintuple"
           onChange={handleOnChangeCheckbox}
         />
-        <Form.Control
-          type="text"
-          name="quintuple"
-          placeholder="$"
-          onChange={handleOnChangeControl}
-          disabled={!currentChecked.includes("quintuple")}
-        />
-      </Form.Group>
-      <Form.Group as={Col} className="p-3">
-        <Form.Check
-          name="niños"
-          type="checkbox"
-          label="Niños"
-          onChange={handleOnChangeCheckbox}
-        />
-
-        <Form.Control
-          type="text"
-          placeholder="$"
-          disabled={!currentChecked.includes("niños")}
-        />
-      </Form.Group>
+        <InputGroup>
+          <InputGroup.Text>$ COP</InputGroup.Text>
+          <Form.Control
+            name="quintuple"
+            onChange={handleOnChangeControl}
+            disabled={!currentChecked.includes("quintuple")}
+          />
+        </InputGroup>
+      </InputGroup>
     </>
   );
 }
