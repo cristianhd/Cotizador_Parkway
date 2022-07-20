4ae59cec4a54548d022db2ee11a79252b9342000
cristianhd <cristian.hd1996@gmail.com>
2022-07-20 16:24:52 -0300

    style login and header

--- cotizador/src/Components/Header.js
+++ cotizador/src/Components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "../assets/header/Logo.png";
-import downButtom from "../assets/header/chevron-circle-down.svg";
+import iconButtom from "../assets/header/chevron-circle-down.svg";
 import "../style/header.css";
 import Login from "./Login";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -11,28 +11,33 @@ import Logo from "./Logo";
 
 export default function Header() {
   const { isLoading, isAuthenticated } = useAuth0();
+  const [stateButtom, setStateButtom] = useState(false);
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  const handleDownButtom = () => {
+    setStateButtom(true);
+    window.scroll({
+      top: 350,
+      behavior: "smooth",
+    });
+  };
+  const handleUpButtom = () => {
+    setStateButtom(false);
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <header>
-      <div className="cont-back">
-        <Login />
-      </div>
-      {isAuthenticated ? <Profile/> : <Logo/>}
-      
-      <div className="down-buttom">
+      <div className="cont-back">{isLoading ? <Loading /> : <Login />}</div>
+      {isAuthenticated ? <Profile /> : <Logo />}
+
+      <div className={stateButtom ? "up-buttom m-3 p-1" : "down-buttom m-3 p-1"}>
         <img
-          src={downButtom}
-          alt="down-buttom"
-          onClick={() =>
-            window.scroll({
-              top: 400,
-              behavior: "smooth",
-            })
-          }
+          src={iconButtom}
+          alt="icon-buttom"
+          onClick={stateButtom ? () => handleUpButtom() : () => handleDownButtom()}
         />
       </div>
     </header>
--- cotizador/src/Components/Login.js
+++ cotizador/src/Components/Login.js
@@ -5,6 +5,7 @@ import loginIcon from "../assets/header/login.svg";
 import logoutIcon from "../assets/header/logout.svg";
 import "../style/login.css";
 import CategoryUser from "./Loading";
+import { Button } from "react-bootstrap";
 
 export default function Login() {
   const {
@@ -16,26 +17,29 @@ export default function Login() {
 
   return (
     <div className="w-100 d-flex justify-content-end">
-      <ul>
-        {isAuthenticated ? (
-          <li className="">
-            <button onClick={logout} className="d-flex justify-content-between">
-              <img src={logoutIcon} alt="login-icon"></img>
-              Cerrar Sesión
-            </button>
-          </li>
-        ) : (
-          <li>
-            <button
-              onClick={loginWithRedirect}
-              className="d-flex justify-content-between"
-            >
-              <img src={loginIcon} alt="logout-icon"></img>
-              Iniciar Sesión
-            </button>
-          </li>
-        )}
-      </ul>
+      {isAuthenticated ? (
+        <Button
+          variant="outline"
+          onClick={logout}
+          className="shadow-none "
+        >
+          <div className="login-button d-flex justify-content-around">
+            <img src={logoutIcon} alt="logout-icon"></img>
+            <span>Cerrar Sesión</span>
+          </div>
+        </Button>
+      ) : (
+        <Button
+          variant="outline"
+          onClick={loginWithRedirect}
+          className="shadow-none p-0 m-0"
+        >
+          <div className="my-5 mx-4 mx-md-5 login-button d-flex justify-content-around">
+            <img src={loginIcon} alt="login-icon"></img>
+            <span>Iniciar Sesión</span>
+          </div>
+        </Button>
+      )}
     </div>
   );
 }
