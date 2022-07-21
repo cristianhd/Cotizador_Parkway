bb72a99bad2c964870c1058a57cf3c84b7b28643
cristianhd <cristian.hd1996@gmail.com>
2022-07-21 04:43:32 -0300

    finish style Footer

--- cotizador/src/Components/Footer.js
+++ cotizador/src/Components/Footer.js
@@ -10,75 +10,93 @@ import facebook from "../assets/footer/Facebook.svg";
 import youtube from "../assets/footer/YouTube.svg";
 import logoFoot from "../assets/footer/logo-foot.png";
 import "../style/footer.css";
+import { Col, Row } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    
-      <footer>
-        <div className="border d-none d-lg-block">
-          <div className="">
+    <>
+      <div className="m-3 p-3">
+        <Row className="w-100 mx-0 my-3 d-none d-lg-block">
+          <Col
+            md={12}
+            className="w-100 m-0 p-0 d-flex flex-row justify-content-center"
+          >
             <img src={clubProducto} alt="club-de-producto"></img>
-          </div>
-          <div className="">
-            <img src={acotur} alt="acotur"></img>
-            <img src={checkIn} alt="check-in"></img>
-            <img src={proColombia} alt="pro-colombia"></img>
-            <img src={safeTravels} alt="afe-travels"></img>
-          </div>
-        </div>
-        <div className="foot">
-          <div className="wrap">
-
-
-            <div className="f-top">
-              <div className="info">
-                <span>
-                  Organizacion Parkway, Cra. 24 # 42-10, Bogotá, Colombia
-                </span>
-              </div>
-              <div className="social-links">
-                <a href="/#">
-                  <img src={instagram} alt="instagram" />
-                </a>
-                <a href="/#">
-                  <img src={facebook} alt="facebook" />
-                </a>
-                <a href="/#">
-                  <img src={whatsapp} alt="whatsapp" />
-                </a>
-                <a href="/#">
-                  <img src={youtube} alt="youtube" />
-                </a>
-              </div>
+          </Col>
+        </Row>
+        <Row className="w-100 mx-0 my-3 p-0 d-flex flex-row justify-content-center align-items-center">
+          <Col md={3} className="m-0 p-0 d-flex align-items-center">
+            <div className="w-100 d-flex justify-content-center">
+              <img src={acotur} alt="acotur"></img>
             </div>
-            <div className="group-links">
-              <div className="links-left">
-                <a href="https://www.colombiateinvita.com">
-                  colombiateinvita.com
-                </a>
-                <a href="https://www.ecoturismocolombia.com">
-                  ecoturismocolombia.com
-                </a>
-                <a href="https://www.parkwayinternacional.com">
-                  parkwayinternacional.com
-                </a>
-              </div>
-              <div className="links-rigth">
-                <a href="/#">Terminos y condiciones</a>
-              </div>
+          </Col>
+          <Col md={3} className="m-0 p-0 d-flex align-items-center">
+            <div className="w-100 d-flex justify-content-center">
+              <img src={checkIn} alt="check-in"></img>
             </div>
-            <div className="f-down">
-              <a href="/">
-                <img src={logoFoot} alt="logo-foot" />
-              </a>
+          </Col>
+          <Col md={3} className="m-0 p-0 d-flex align-items-center">
+            <div className="w-100 d-flex justify-content-center">
+              <img src={proColombia} alt="pro-colombia"></img>
+            </div>
+          </Col>
+          <Col md={3} className="m-0 p-0 d-flex align-items-center">
+            <div className="w-100 d-flex justify-content-center">
+              <img src={safeTravels} alt="afe-travels"></img>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <div className="foot">
+        <div className="wrap">
+          <div className="f-top">
+            <div className="info">
               <span>
-                Copyright 2021 © Organizacion Parkway | Todos los derechos
-                reservados.
+                Organizacion Parkway, Cra. 24 # 42-10, Bogotá, Colombia
               </span>
             </div>
+            <div className="social-links">
+              <a href="/#">
+                <img src={instagram} alt="instagram" />
+              </a>
+              <a href="/#">
+                <img src={facebook} alt="facebook" />
+              </a>
+              <a href="/#">
+                <img src={whatsapp} alt="whatsapp" />
+              </a>
+              <a href="/#">
+                <img src={youtube} alt="youtube" />
+              </a>
+            </div>
+          </div>
+          <div className="group-links">
+            <div className="links-left">
+              <a href="https://www.colombiateinvita.com">
+                colombiateinvita.com
+              </a>
+              <a href="https://www.ecoturismocolombia.com">
+                ecoturismocolombia.com
+              </a>
+              <a href="https://www.parkwayinternacional.com">
+                parkwayinternacional.com
+              </a>
+            </div>
+            <div className="links-rigth">
+              <a href="/#">Terminos y condiciones</a>
+            </div>
+          </div>
+          <div className="f-down">
+            <a href="/">
+              <img src={logoFoot} alt="logo-foot" />
+            </a>
+            <span>
+              Copyright 2021 © Organizacion Parkway | Todos los derechos
+              reservados.
+            </span>
           </div>
         </div>
-      </footer>
-
+      </div>
+    </>
   );
 }
