7b938c15d619bdea92530db568fc3ac07cbf065a
cristianhd <cristian.hd1996@gmail.com>
2022-07-27 02:19:39 -0300

    style suggest and config scss

--- cotizador/src/Components/Cards.js
+++ cotizador/src/Components/Cards.js
@@ -6,7 +6,7 @@ import NewProduct from "./NewProduct";
 
 export default function Cards({ data, typeProduct }) {
   useEffect(() => {
-    window.scroll({ top: 1000, behavior: "smooth" });
+    window.scroll({ top: 750, behavior: "smooth" });
   },[data]);
   return (
     <div className="cards-flex m-auto gap-5">
--- cotizador/src/Components/SuggestPlaces.js
+++ cotizador/src/Components/SuggestPlaces.js
@@ -3,11 +3,11 @@ import { Button, ButtonGroup } from "react-bootstrap";
 
 export default function SuggestPlaces({ suggest, onClick, name }) {
   return (
-    <div className="d-fle flex-column">
+    <div className="suggest d-fle flex-column">
       {suggest &&
         suggest.map((place, index) => {
           return (
-            <Button className="w-100 rounded-0" key={index} variant="dark" onClick={() => onClick(name, place.name)}>
+            <Button className="w-100 rounded-0 bg-gray" key={index} variant="dark" onClick={() => onClick(name, place.name)}>
               {place.name}
             </Button>
           );
