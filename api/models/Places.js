0358c0d875c03e366e1531fd9afc10bd14ba9bf4
cristianhd <cristian.hd1996@gmail.com>
2022-09-13 22:14:52 -0300

    change structure model places

--- api/models/Places.js
+++ api/models/Places.js
@@ -1,7 +1,9 @@
 const { Schema, model } = require("mongoose");
 
 const placeSchema = new Schema({
-  name: { type: String, require: true, unique: true }  
+  name: { type: String, require: true, unique: true },
+  typePlace : {type: String, require: true},
+  city: {type: String, require: true}
 });
 
 const Lugares = model("lugares", placeSchema);
