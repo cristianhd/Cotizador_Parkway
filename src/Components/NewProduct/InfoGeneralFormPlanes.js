923d075421c86fd8b6fc47c17ca15fa61233fb86
cristianhd <cristian.hd1996@gmail.com>
2022-11-23 19:22:08 -0300

    create first step form navigation

--- src/Components/NewProduct/InfoGeneralFormPlanes.js
+++ src/Components/NewProduct/InfoGeneralFormPlanes.js
@@ -1,14 +1,48 @@
-import React, { useState } from "react";
-import { Col, Form, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
 import FloatingInput from "./FloatingInput";
 
-export default function InfoGeneralFormPlanes({ handleOnChangeForm, form }) {
-  // const [form, setForm] = useState({
-  //   title: "",
-  //   destination: "",
-  //   transport: "",
-  //   providerUser: "",
-  // });
+export default function InfoGeneralFormPlanes({
+  handleOnChangeForm,
+  handleOnChangeDestination,
+  form,
+}) {
+  const [checkedMultiDestination, updateCheckedMultiDestination] =
+    useState(false);
+  const [multiDestination, addMultiDestination] = useState([1]);
+  const [newDestination, setNewDestination] = useState({});
+
+  function handleOnChangeCheck(e) {
+    const checked = e.target.checked;
+    if (checked) {
+      addMultiDestination([...multiDestination, 2]);
+    } else {
+      addMultiDestination([1]);
+    }
+    updateCheckedMultiDestination(checked);
+  }
+
+  function AddDestination() {
+    addMultiDestination([...multiDestination, multiDestination.length + 1]);
+  }
+  function RemoveDestination() {
+    const label = "destino-" + multiDestination[multiDestination.length - 1];
+    const destination = undefined;
+
+    addMultiDestination(multiDestination.slice(0, multiDestination.length - 1));
+    setNewDestination({ ...newDestination, [label]: " " });
+    handleOnChangeDestination(label, destination);
+  }
+  function handleOnChangeMultiDestination(e) {
+    const label = e.target.name;
+    const destination = e.target.value;
+    setNewDestination({ ...newDestination, [label]: destination });
+    handleOnChangeDestination(label, destination);
+  }
+
+  // handleOnChangeDestination(newDestination);
+
+  console.log(newDestination);
   return (
     <>
       <Row className="m-1">
@@ -24,14 +58,30 @@ export default function InfoGeneralFormPlanes({ handleOnChangeForm, form }) {
 
       <Row className="m-1">
         <Form.Group className="" as={Col}>
-          <FloatingInput
-            name="destination"
-            labelName="Destino"
-            value={form.destination}
-            onChange={(e) => handleOnChangeForm(e)}
-          />
+          {multiDestination.map((destination, index) => (
+            <FloatingInput
+              key={index}
+              name={`destino-${destination}`}
+              labelName={`Destino ${destination}`}
+              value={Object.values(form.destination)[index]}
+              onChange={(e) => handleOnChangeMultiDestination(e)}
+            />
+          ))}
+          <div className="p-1 d-flex justify-content-between">
+            {checkedMultiDestination && multiDestination.length > 1 && (
+              <span onClick={RemoveDestination}> - eliminar destino</span>
+            )}
+            {checkedMultiDestination && multiDestination.length < 5 && (
+              <span onClick={AddDestination}> + agregar destino</span>
+            )}
+          </div>
           <Form.Group className="ps-1">
-            <Form.Check type="switch" label="Multidestino" />
+            <Form.Check
+              type="switch"
+              label="Multidestino"
+              onChange={handleOnChangeCheck}
+              checked={multiDestination.length > 1}
+            />
           </Form.Group>
         </Form.Group>
       </Row>
