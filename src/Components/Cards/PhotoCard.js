16eed2303d8a717a3e0482112b44c08ef724689a
cristianhd <cristian.hd1996@gmail.com>
2022-12-12 21:38:46 -0300

    style card photo

--- src/Components/Cards/PhotoCard.js
+++ src/Components/Cards/PhotoCard.js
@@ -4,13 +4,14 @@ import "../../style/card.css";
 
 export default function PhotoCard({ pricipalText, photo, listSpanText }) {
   return (
-    <div className="photo-card  d-flex-column">
+    <div className="photo-card">
       <img className="img-card" src={photo} alt="photoCard"></img>
-      <div className="text-card p-1">
+      <div className="text-card p-2 d-flex flex-column justify-content-center">
         <h3>{pricipalText}</h3>
-        {listSpanText.map((text) => (
-          <SpanCard text={text}></SpanCard>
-        ))}
+        <div className="m-1 span-card d-flex flex-row">
+          {listSpanText &&
+            listSpanText.map((text) => <SpanCard text={text}></SpanCard>)}
+        </div>
       </div>
     </div>
   );
