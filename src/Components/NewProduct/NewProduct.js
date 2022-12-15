8039dad1f2fccdf6455c4c4208a0591e773fdef1
cristianhd <cristian.hd1996@gmail.com>
2022-12-15 14:08:47 -0300

    delete card header newProduct

--- src/Components/NewProduct/NewProduct.js
+++ src/Components/NewProduct/NewProduct.js
@@ -4,13 +4,13 @@ import ModalNewProduct from "./ModalNewProduct";
 
 export default function NewProduct({ typeProduct }) {
   return (
-    <Card className="p-1">
-      <Card.Header className="w-100">
+    <Card className="w-25 p-1">
+      {/* <Card.Header className="w-100">
         <div className="container-img d-flex justify-content-center">
           <Card.Img variant="top" src={photos} />
         </div>
-      </Card.Header>
-      <Card.Body className="w-100">
+      </Card.Header> */}
+      <Card.Body className="">
         <ModalNewProduct typeProduct={typeProduct} />
       </Card.Body>
     </Card>
