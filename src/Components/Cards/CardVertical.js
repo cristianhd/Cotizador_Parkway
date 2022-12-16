e59feb2fe00414dd6f0750e562b4d23cb21f3aeb
cristianhd <cristian.hd1996@gmail.com>
2022-12-16 14:05:30 -0300

    add destructuring listSpanText

--- src/Components/Cards/CardVertical.js
+++ src/Components/Cards/CardVertical.js
@@ -3,20 +3,26 @@ import NavigationCard from "./NavigationCard";
 import PhotoCard from "./PhotoCard";
 
 export default function CardVertical({
+  title,
+  photo,
+  categoryAccommodation,
+  destinationName,
+  numberNigths,
+  description,
+  transport,
   priceKids,
   priceAdult,
-  roundTrip,
   typeProduct,
-  description,
+  roundTrip,
   listSpanText,
 }) {
   return (
     <div className="card-vertical">
       <div className="d-flex flex-column w-100">
         <PhotoCard
-          photo="https://i.pinimg.com/564x/dc/a2/04/dca2046e6525ed56ca76c76f724cae0c.jpg"
-          pricipalText="Conoce la Güajira"
-          subtitleText="Guajira,Colombia"
+          photo={photo}
+          pricipalText={title}
+          subtitleText={destinationName}
           listSpanText={listSpanText}
         ></PhotoCard>
         <NavigationCard
