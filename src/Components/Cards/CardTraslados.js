dc7d636c61e8b3ffc94aac0d35b9ecbe563ad24a
cristianhd <cristian.hd1996@gmail.com>
2022-12-19 12:48:02 -0300

    created card vertical

---
+++ src/Components/Cards/CardTraslados.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CardVertical from "./CardVertical";
+
+export default function CardTraslados({ data, typeProduct }) {
+  console.log(data);
+  return (
+    <div className="d-flex flex-column">
+      {data.length > 0 &&
+        data.map((card, index) => (
+          <CardVertical
+            photo="https://i.pinimg.com/564x/23/de/c8/23dec89bc595891cdce353b7ec905fd4.jpg"
+            key={index}
+            title={card.title}
+            priceKids={card.priceKids}
+            priceAdult={card.priceAdult}
+            typeProduct={typeProduct}
+            description={card.description}
+            roundTrip={card.roundTrip}
+          ></CardVertical>
+        ))}
+    </div>
+  );
+}
