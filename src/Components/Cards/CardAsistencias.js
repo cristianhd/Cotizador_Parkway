ec3257a30d1bbccf68ca525514936880feb908de
cristianhd <cristian.hd1996@gmail.com>
2022-12-19 14:52:04 -0300

    key index to child map

--- src/Components/Cards/CardAsistencias.js
+++ src/Components/Cards/CardAsistencias.js
@@ -7,7 +7,7 @@ export default function CardAsistencias({ data, typeProduct }) {
     <div className="d-flex flex-column">
       {data.length > 0 &&
         data.map((card, index) => (
-          <div className="card-vertical">
+          <div className="card-vertical" key={index}>
             <div className="d-flex flex-column w-100">
               <PhotoCard
                 photo="https://i.pinimg.com/564x/32/43/ee/3243ee7b772f95d751578e43e23bc443.jpg"
