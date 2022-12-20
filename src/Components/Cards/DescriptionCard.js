d8e5b61d7bcab4d5bf43c25dcd7361d06e5a74df
cristianhd <cristian.hd1996@gmail.com>
2022-12-20 12:02:48 -0300

    spacing description component

--- src/Components/Cards/DescriptionCard.js
+++ src/Components/Cards/DescriptionCard.js
@@ -3,10 +3,10 @@ import { Card, Col, Row } from "react-bootstrap";
 
 export default function DescriptionCard({ description }) {
   return (
-    <Card.Body className="w-100 m-2 p-2" as={Row}>
-      <Col className="m-1">
+    <Card.Body className="m-1 p-1" as={Row}>
+      <Col>
         <Card.Title>Descripción</Card.Title>
-        <div className="m-1">{description}</div>
+        <div className="m-1 description">{description}</div>
       </Col>
     </Card.Body>
   );
