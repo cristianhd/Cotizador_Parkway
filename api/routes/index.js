d8a779cc460b1df21aa1a9b407326c755cef0cd3
cristianhd <cristian.hd1996@gmail.com>
2023-02-12 01:14:01 -0300

    file missing in commit create routes photos

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,11 +5,13 @@ const usersRoutes = require("./users");
 const productsRoutes = require("./products.js");
 const placesRoutes = require("./places.js");
 const citiesRoutes = require("./cities.js");
+const photosRoutes = require("./photos.js");
 const { VerifyToken } = require("../controllers/protected");
 
 router.use("/users", VerifyToken, usersRoutes);
 router.use("/products", productsRoutes);
 router.use("/places", placesRoutes);
 router.use("/cities", citiesRoutes);
+router.use("/photos", photosRoutes);
 
 module.exports = router;
