299ab12110afb57f8c6d5a2e4098d15c48cee78a
cristianhd <cristian.hd1996@gmail.com>
2023-02-16 23:24:08 -0300

    create photos and includes logical control form

---
+++ src/Components/NewProduct/Hospedajes/StepFourFormHospedajes.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Col, FloatingLabel, Form, Row } from "react-bootstrap";
+import Includes from "../Includes";
+
+export default function StepFourFormHospedajes({
+  form,
+  includesForm,
+  handleOnChangeForm,
+  handleOnChangeIncludes,
+}) {
+  return (
+    <>
+      <Row className="m-1">
+        <Form.Group as={Col} className="p-3">
+          <FloatingLabel label="Descripción" className="m-1">
+            <Form.Control
+              required
+              className="shadow-none"
+              as="textarea"
+              maxLength="500"
+              placeholder="Escribe una descripción"
+              name="description"
+              value={form.description}
+              onChange={handleOnChangeForm}
+            />
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group className="p-3">
+          <Includes
+            handleOnChangeIncludes={handleOnChangeIncludes}
+            form={form}
+            includesForm={includesForm}
+          />
+        </Form.Group>
+      </Row>
+    </>
+  );
+}
