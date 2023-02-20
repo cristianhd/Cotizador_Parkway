ad1fa8446add2811f8eb1d2c7a99651319b64523
cristianhd <cristian.hd1996@gmail.com>
2023-02-20 20:25:54 -0300

    italic locatio

--- src/Components/Cards/CaptionPhotoCard.js
+++ src/Components/Cards/CaptionPhotoCard.js
@@ -20,15 +20,15 @@ export default function CaptionPhotoCard({
       <div className="location">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
+          width="14"
+          height="14"
           fill="gray"
           class="bi bi-geo-alt-fill"
           viewBox="0 0 16 16"
         >
           <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
         </svg>
-        <span className="m-1">{subtitleText && subtitleText}</span>
+        <span className="m-1 fst-italic">{subtitleText && subtitleText}</span>
       </div>
     </div>
   );
