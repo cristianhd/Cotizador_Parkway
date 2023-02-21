0e93da98abddec507efdaaa30efdb2a59d554f60
cristianhd <cristian.hd1996@gmail.com>
2023-02-21 02:33:12 -0300

    refactor: deleteProduct

--- src/Components/Cards/CardHorizontal.js
+++ src/Components/Cards/CardHorizontal.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { deleteProduct, getProductbyId } from "../../Redux/action";
-import FooterCard from "./FooterDescription";
+import DeleteProduct from "./DeleteProduct";
 import ModalDelete from "./ModalDelete";
-import ModalEdit from "./ModalEdit";
 import NavigationCard from "./NavigationCard";
 import PhotoCard from "./PhotoCard";
 
@@ -28,30 +26,12 @@ export default function CardHorizontal({
     destinationName.map((destination, index) =>
       destinationName.length - 1 === index ? destination : `${destination} - `
     );
-  const [showDelete, setShowDelete] = useState();
-  const [showEdit, setShowEdit] = useState();
-  const dispatch = useDispatch();
-
-  function handleOnDelete() {
-    dispatch(deleteProduct(id, typeProduct));
-    setShowDelete(false);
-  }
-
-  function handleModalEdit() {
-    dispatch(getProductbyId(id, typeProduct));
-    setShowEdit(true);
-  }
-  function handleShowDelete() {
-    setShowDelete(false);
-  }
-  function handleShowEdit() {
-    setShowEdit(false);
-  }
 
   return (
     <div className="card-horizontal ">
       <div className="d-flex flex-column flex-sm-column flex-md-row">
         <PhotoCard
+          id={id}
           photo={photo}
           principalText={title}
           subtitleText={
@@ -60,6 +40,7 @@ export default function CardHorizontal({
           typeProduct={typeProduct}
         ></PhotoCard>
         <NavigationCard
+          id={id}
           priceKids={priceKids}
           priceAdult={priceAdult}
           typeProduct={typeProduct}
@@ -70,24 +51,7 @@ export default function CardHorizontal({
           maxPeople={maxPeople}
           minPeople={minPeople}
         ></NavigationCard>
-        <div className=" delete-horizontal m-1 p-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="#C0C0C0"
-            className="span-pointer bi bi-x-circle-fill"
-            viewBox="0 0 16 16"
-            onClick={() => setShowDelete(true)}
-          >
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-          </svg>
-        </div>
-        <ModalDelete
-          showModal={showDelete}
-          handleOnDelete={handleOnDelete}
-          handleShowDelete={handleShowDelete}
-        ></ModalDelete>
+        <DeleteProduct id={id} typeProduct={typeProduct} />
       </div>
     </div>
   );
